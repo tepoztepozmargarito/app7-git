c03f3a9058760e5106adb5118fc5514724549fdd
yahir <yahirruizmen0@gmail.com>
2019-05-13 09:08:22 -0500

    1subida

---
+++ js/acciones.js
@@ -0,0 +1,50 @@
+// JavaScript Document
+
+$(document).ready(function(e) {
+    document.addEventListener("deviceready",function(){
+		$('#disp table td').eq(3).text(device.model);
+		$('#disp table td').eq(3).text(device.cordova);
+		$('#disp table td').eq(3).text(device.platform);
+		$('#disp table td').eq(3).text(device.version);
+		$('#disp table td').eq(3).text(device.uuid);
+	},false);//ready device
+
+});//document
+
+$(document).ready(function(e){
+document.addEventListener("deviceready",function(){
+		
+$('#beep').tap(function(){
+navigator.notification.beep(1);
+});//tap beep
+
+$('#vibrar').tap(function(){
+navigator.notification.vibrate(1000);
+});//tap vibrar
+
+},false); //deviceready
+}); //ready
+
+$(document).ready(function(e){
+document.addEventListener("deviceready",function(){
+	
+	$('#izquierda').on("swipeleft",function(){
+		navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion","Aceptar"});
+	});
+	$('#derecha').on("swiperight",function(){
+		navigator .notification. confirm ("¿que quieres hacer?",function(opt){
+			switch(opt)
+			{
+				case 1:
+				navigator.notification.beep(1);
+				break;
+				
+				case 2:
+				navigator.notification.vibrate(1000);
+				break;
+			}
+		},"Aplicacion ","Beep,Vibrar,Cancelar");
+	});
+},false);
+});
+	
