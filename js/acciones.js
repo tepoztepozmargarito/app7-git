33914ca9c51547e876cbb4b1126732e135a017fe
yahir <yahirruizmen0@gmail.com>
2019-05-13 09:50:41 -0500

    2

--- js/acciones.js
+++ js/acciones.js
@@ -1,14 +1,14 @@
 // JavaScript Document
 
-$(document).ready(function(e) {
-    document.addEventListener("deviceready",function(){
+$(document).ready(function (e){
+	document.addEventListener ("deviceready",function(){
 		$('#disp table td').eq(3).text(device.model);
-		$('#disp table td').eq(3).text(device.cordova);
-		$('#disp table td').eq(3).text(device.platform);
-		$('#disp table td').eq(3).text(device.version);
-		$('#disp table td').eq(3).text(device.uuid);
+		$('#disp table td').eq(5).text(device.cordova);
+		$('#disp table td').eq(7).text(device.platform);
+		$('#disp table td').eq(9).text(device.version);
+		$('#disp table td').eq(11).text(device.uuid);
 	},false);//ready device
-
+	
 });//document
 
 $(document).ready(function(e){
